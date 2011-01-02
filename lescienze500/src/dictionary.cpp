7e7af0b957d88c263c607f33962d0b9d4f2c9943
Simone Riva <simone.rva@gmail.com>
2011-01-02 16:31:59 +0100

    fatto generatore hash dizionario

--- lescienze500/src/dictionary.cpp
+++ lescienze500/src/dictionary.cpp
@@ -63,6 +63,8 @@ bool Dictionary::buildDictionary()
     this->addCategorie() ;
     this->addIdArticoli() ;
 
+    this->dictionary.clear();
+
     QString query_art_id = "select Id from Articoli" ;
     configLS500 cfg ;
 
@@ -71,11 +73,13 @@ bool Dictionary::buildDictionary()
 
     db.execQuery( cfg.getDBPath() , query_art_id , id_articoli ) ;
 
+    int e_cnt = 0 ;
     for ( QueryResult::iterator itr = id_articoli.begin() ; itr < id_articoli.end() ; itr++ )
     {
 
         QString id = id_articoli.getField( "Id" , itr ) ;
         qDebug() << id ;
+        qDebug() << this->dictionary.size() ;
 
         QString query_testo_esteso = "select TestoEsteso from RicercaEstesa where IdArticolo = " ;
         query_testo_esteso += id ;
@@ -105,26 +109,46 @@ bool Dictionary::buildDictionary()
         }
 
         this->addText( full_text , id ) ;
-        return true ;
+        this->fillDataBase() ;
+
+        if( e_cnt++ == 100 ) return true ;
     }
 
+    this->dictionary.clear();
     return true ;
 }
 
 bool Dictionary::addText( QString text , QString id_art )
 {
+    QueryDB db ;
+    QueryResult anno ;
+    QueryResult categorie ;
+
     QRegExp reg_word ;
     reg_word.setPattern( "([a-zA-Z]+)" );
-    QueryDB db ;
 
     QString word ;
-    QHash<QString,int> article_dict ;
 
     configLS500 cfg ;
     QString db_path = cfg.getDictionaryPath() ;
     db_path.replace( QRegExp( "(^\\$HOME)" ) , QDir::homePath() ) ;
 
+    QString query_categoria = "select Categoria from categorie where id in ( select idcategoria from articoli_categorie where idarticolo = " ;
+    query_categoria += id_art  ;
+    query_categoria += " )"  ;
 
+    db.execQuery( cfg.getDBPath() , query_categoria , categorie ) ;
+
+    QString query_anno = "SELECT anno FROM riviste WHERE id in ( " ;
+    query_anno += " select  IdRivista from articoli where Id = " ;
+    query_anno += id_art ;
+    query_anno += " ) " ;
+
+    db.execQuery( cfg.getDBPath() , query_anno , anno ) ;
+
+    int year ;
+    year = anno.getField(0,0).toInt() ;
+    //qDebug() << year ;
 
     int count = 0;
     int pos = 0;
@@ -133,54 +157,39 @@ bool Dictionary::addText( QString text , QString id_art )
         count++;
         word = text.mid( pos , reg_word.matchedLength() ) ;
 
-        QString select_wcnt = "select Cnt from words where Word like \"" ;
-        select_wcnt =  word ; select_wcnt =  "\"" ;
+        WordInfo w_info = this->dictionary.value( word , WordInfo() ) ;
 
-        QueryResult qr_word ;
-        db.execQuery( db_path , select_wcnt , qr_word ) ;
+        w_info.word = word ;
+        w_info.cnt++ ;
 
-        int w_cnt = article_dict.value( word , 0 ) ;
+        QPair<int,int> info_year = w_info.year_cnt.value( year , QPair<int,int>(0,0) ) ;
+        info_year.first = year ;
+        info_year.second++ ;
 
-        if( !qr_word.empty() )
+        //qDebug() << word << " " << w_info.cnt << " " << year << " " << info_year.second ;
+
+        w_info.year_cnt.insert( year , info_year ) ;
+
+        for( QueryResult::iterator itr = categorie.begin() ; itr < categorie.end() ; itr++ )
         {
-            w_cnt += qr_word.getField(0,0).toInt() ;
+            int cat_id = categorie.getField(0,itr).toInt() ;
+            QPair<int,int> info_cat = w_info.categoriaid_cnt.value( cat_id , QPair<int,int>(0,0) ) ;
+            info_cat.first = cat_id ;
+            info_cat.second++ ;
+            w_info.categoriaid_cnt.insert( cat_id , info_cat ) ;
         }
 
-        article_dict.insert( word , w_cnt+1 ) ;
+        this->dictionary.insert( word , w_info ) ;
         pos += reg_word.matchedLength() ;
     }
 
-    QString query_categoria = "select categoria from categorie where id in ( select idcategoria from articoli_categorie where idarticolo = " ;
-    query_categoria += id_art  ;
-    query_categoria += " )"  ;
-
-    QString query_anno = "SELECT anno FROM riviste WHERE id in ( " ;
-    query_anno += " select  IdRivista from articoli where Id = " ;
-    query_anno += id_art ;
-    query_anno += " ) " ;
-
-    QString global_insert = "  begin ; " ;
-
-    QList<QString> keys =  article_dict.keys() ;
-    for ( QList<QString>::iterator itr = keys.begin() ; itr != keys.end() ; itr++ )
-    {
-        QString inser_word = "" ;
-
-        inser_word =  " insert into Words ( Word , Cnt ) values (  \"" ;
-        inser_word += *itr ;
-        inser_word += "\" , " ;
-        inser_word +=  QString().setNum( article_dict.value( *itr ) ) ;
-        inser_word += " ) ; " ;
-        qDebug() << inser_word ;
-
-        global_insert += inser_word ;
-    }
-
-    global_insert += " commit ; " ;
-    return QueryDB::execNAQuery( db_path , global_insert ) ;
-
+    return true ;
 }
 
+bool Dictionary::fillDataBase()
+{
+
+}
 
 bool Dictionary::addYears()
 {
--- lescienze500/src/dictionary.h
+++ lescienze500/src/dictionary.h
@@ -2,21 +2,44 @@
 #define DICTIONARY_H
 
 #include <QString>
+#include <QHash>
+#include <QPair>
+#include <QList>
+#include <QObject>
 
-class Dictionary
+class WordInfo
 {
+public:
+    WordInfo() { cnt = 0 ; }
+    QString word ;
+    int cnt ;
+    QHash< int , QPair<int,int> > year_cnt ;
+    QHash< int , QPair<int,int> > categoriaid_cnt ;
+    QHash< int , QPair<int,int> > articoloid_cnt ;
+};
+
+class Dictionary : public QObject
+{
+    Q_OBJECT
 public:
     Dictionary();
 
     bool initDictionary() ;
     bool buildDictionary() ;
+
     bool addText( QString text , QString id_art ) ;
+    bool fillDataBase() ;
     bool exists() ;
 
     bool addYears() ;
     bool addCategorie() ;
     bool addIdArticoli() ;
 private:
+
+    QHash<QString,WordInfo> dictionary ;
+
+signals:
+    void sig_progress( int cnt , int total ) ;
 
 };
 
